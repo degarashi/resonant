f33ed33dcf5519617ea534ded9157958cd7140ac
degarashi <slice013@gmail.com>
2013-11-08 15:10:07 +0900

    gameloop の内部処理をユーザーが置き換えられるようにした

--- gameloop.cpp
+++ gameloop.cpp
@@ -5,8 +5,8 @@
 
 namespace rs {
 	// --------------------- DrawThread ---------------------
-	void DrawThread::runL(Looper& drawLooper, const SPWindow& w) {
-		Handler drawHandler(drawLooper);
+	void DrawThread::runL(Looper& mainLooper, const SPWindow& w) {
+		Handler drawHandler(mainLooper);
 		drawHandler.postArgs(msg::DrawInit());
 
 		SPGLContext ctx = GLContext::CreateContext(w);
@@ -15,19 +15,14 @@ namespace rs {
 		mgr_gl.onDeviceReset();
 
 		bool bLoop = true;
-		int count=0;
 		do {
 			// メインスレッドから描画開始の指示が来るまで待機
 			while(auto m = getLooper()->wait()) {
 				if(msg::DrawReq* p = *m) {
 					setState(1);
-
 					// 1フレーム分の描画処理
-					glClearColor(0,0,(count&0xff) / 255.0f,1);
-					count ++;
-					glClear(GL_COLOR_BUFFER_BIT);
+					runU(_accum++);
 					ctx->swapWindow();
-
 					setState(0);
 				}
 			}
@@ -44,15 +39,24 @@ namespace rs {
 
 	// --------------------- MainThread ---------------------
 	void MainThread::runL(Looper& guiLooper, const SPWindow& w) {
-		Handler guiHandler(guiLooper);
+		GLRes			glrI;
+		RWMgr			rwI;
+		FontFamily		fontI;
+		fontI.loadFamilyWildCard("/home/slice/.fonts/*.ttc");
+		FontGen			fgenI(spn::PowSize(512,512));
+		InputMgr 		inpI;
 
-		_dth.start(std::ref(*getLooper()), w);
+		initU();
+
+		Handler guiHandler(guiLooper);
+		_dth = initDraw();
+		_dth->start(std::ref(*getLooper()), w);
 		// 描画スレッドの初期化完了を待つ
 		while(auto m = getLooper()->wait()) {
 			if(msg::DrawInit* p = *m)
 				break;
 		}
-		Handler drawHandler(*_dth.getLooper());
+		Handler drawHandler(*_dth->getLooper());
 		guiHandler.postArgs(msg::MainInit());
 
 		const spn::FracI fracInterval(50000, 3);
@@ -61,13 +65,6 @@ namespace rs {
 		int skip = 0;
 		constexpr int MAX_SKIPFRAME = 3;
 		constexpr int DRAW_THRESHOLD_USEC = 2000;
-
-		InputMgr imgr;
-		HLInput hlIk = Keyboard::OpenKeyboard();
-		auto actQuit = imgr.addAction("quit");
-		imgr.link(actQuit.get(), InF::AsButton(hlIk.get(), SDL_SCANCODE_ESCAPE));
-		auto actButton = imgr.addAction("button");
-		imgr.link(actQuit.get(), InF::AsButton(hlIk.get(), SDL_SCANCODE_A));
 
 		using namespace std::chrono;
 		// ゲームの進行や更新タイミングを図って描画など
@@ -89,7 +86,7 @@ namespace rs {
 
 			// ゲーム進行
 			mgr_input.update();
-			if(mgr_input.isKeyPressed(actQuit.get()))
+			if(!runU())
 				break;
 
 			// 時間が残っていれば描画
@@ -103,26 +100,20 @@ namespace rs {
 		} while(bLoop && !isInterrupted());
 
 		// 描画スレッドの終了を待つ
-		_dth.interrupt();
-		_dth.join();
+		_dth->interrupt();
+		_dth->join();
 	}
 
-	int GameLoop(spn::To8Str title, int w, int h, uint32_t flag, int major, int minor, int depth) {
+	int GameLoop(UPMainTh&& mth, spn::To8Str title, int w, int h, uint32_t flag, int major, int minor, int depth) {
 		SDLInitializer	sdlI(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_JOYSTICK | SDL_INIT_TIMER);
-		GLRes			glrI;
-		RWMgr			rwI;
-		FontFamily		fontI;
-		fontI.loadFamilyWildCard("/home/slice/.fonts/*.ttc");
-		FontGen			fgenI(spn::PowSize(512,512));
-
 		Window::SetStdGLAttributes(major, minor, depth);
 		SPWindow _spWindow = Window::Create(title.moveTo(), w, h, flag);
+
 		// メインスレッドのメッセージキューを初期化
 		Looper::Prepare();
 		auto& loop = Looper::GetLooper();
 		// 描画スレッドに渡す
-		MainThread mth;
-		mth.start(std::ref(loop), std::ref(_spWindow));
+		mth->start(std::ref(loop), std::ref(_spWindow));
 		// 描画スレッドのキューが準備出来るのを待つ
 		while(auto msg = loop.wait()) {
 			if(msg::MainInit* p = *msg)
@@ -131,7 +122,7 @@ namespace rs {
 		// GUIスレッドのメッセージループ
 		SDL_Event e;
 		bool bLoop = true;
-		while(bLoop && mth.isRunning() && SDL_WaitEvent(&e)) {
+		while(bLoop && mth->isRunning() && SDL_WaitEvent(&e)) {
 			if(e.type == EVID_SIGNAL) {
 				// 自作スレッドのキューにメッセージがある
 				while(OPMessage m = loop.peek(std::chrono::seconds(0))) {
@@ -168,8 +159,8 @@ namespace rs {
 				}
 			}
 		}
-		mth.interrupt();
-		mth.join();
+		mth->interrupt();
+		mth->join();
 		return 0;
 	}
 }
--- gameloop.hpp
+++ gameloop.hpp
@@ -4,6 +4,8 @@
 #include "spinner/abstbuff.hpp"
 
 namespace rs {
+	template <class T>
+	using UPtr = std::unique_ptr<T>;
 	namespace msg {
 		struct DrawInit : MsgBase {};
 		struct MainInit : MsgBase {};
@@ -20,20 +22,29 @@ namespace rs {
 	class DrawThread : public ThreadL<void (Looper&, const SPWindow&)> {
 		using base = ThreadL<void (Looper&, const SPWindow&)>;
 		int 			_state = 0;
+		uint64_t		_accum = 0;
 		mutable Mutex	_mutex;
 		protected:
-			void runL(Looper& drawLooper, const SPWindow& w) override;
+			void runL(Looper& mainLooper, const SPWindow& w) override;
 			void setState(int s);
+			virtual void runU(uint64_t accum) {}
 		public:
 			int getState() const;
 	};
+	using UPDrawTh = UPtr<DrawThread>;
+
 	class MainThread : public ThreadL<void (Looper&,const SPWindow&)> {
 		using base = ThreadL<void (Looper&, const SPWindow&)>;
-		DrawThread	_dth;
+		UPDrawTh	_dth;
 		protected:
 			void runL(Looper& guiLooper, const SPWindow& w) override;
+			virtual bool runU() { return true; }
+			virtual void initU() {}
+			virtual UPDrawTh initDraw() { return UPDrawTh(new DrawThread); }
 		public:
 	};
+	using UPMainTh = UPtr<MainThread>;
+
 	const uint32_t EVID_SIGNAL = SDL_RegisterEvents(1);
-	int GameLoop(spn::To8Str title, int w, int h, uint32_t flag, int major=2, int minor=0, int depth=16);
+	int GameLoop(UPMainTh&& mth, spn::To8Str title, int w, int h, uint32_t flag, int major=2, int minor=0, int depth=16);
 }
