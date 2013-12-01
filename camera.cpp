b1eb318abb1d8f0e85e68dc928b302b0f7cfbbb9
degarashi <slice013@gmail.com>
2013-12-01 13:32:16 +0900

    Cameraを追加

---
+++ camera.cpp
@@ -0,0 +1,230 @@
+#include "camera.hpp"
+
+namespace rs {
+	bool CamData::_checkAC(uint32_t& acDst) const {
+		uint32_t ac = getAccum();
+		if(acDst != ac) {
+			acDst = ac;
+			return true;
+		}
+		return false;
+	}
+
+	void CamData::_calcMatrices() const {
+		if(_checkAC(_acMat)) {
+			// 回転を一端quatに変換
+			const Quat& q = getRot();
+			_matV.identity();
+			_matV.getRow(3) = -getOffset();
+
+			Quat tq = q.inverse();
+			_matV *= tq.asMat33();
+
+			_matP = AMat44::PerspectiveFovLH(_fov, _aspect, _nearZ, _farZ);
+			_matVP = _matV * _matP;
+		}
+	}
+	void CamData::_calcVPInv() const {
+		if(_checkAC(_acMatInv)) {
+			_calcMatrices();
+			_matVP.inversion(_matVPInv);
+		}
+	}
+	void CamData::_calcFrustum() const {
+		if(_checkAC(_acVFrus)) {
+			// UP軸とZ軸を算出
+			AMat44 mat;
+			getViewMatrix().convertA44().inversion(mat);
+			mat.getRow(3) = AVec4(0,0,0,1);
+			AVec4 zAxis(0,0,1,0),
+				yAxis(0,1,0,0);
+			zAxis *= mat;
+			yAxis *= mat;
+
+			_vfrus = Frustum(getOffset(), zAxis.asVec3(), yAxis.asVec3(), _fov, _farZ, _aspect);
+		}
+	}
+
+	CamData::CamData():
+		Pose3D(Vec3(0,0,-10), Quat(0,0,0,1), Vec3(1,1,1)),
+		_accum(1), _acMat(0), _acMatInv(0), _acVFrus(0)
+	{
+		_nearZ = 1.f;
+		_farZ = 1e5f;
+		_fov = spn::DEGtoRAD(90.0f);
+		_aspect = 1.4f / 1.f;
+	}
+	CamData::CamData(const CamData& c): Pose3D(static_cast<const Pose3D&>(c)), _matV() {
+		uintptr_t ths = reinterpret_cast<uintptr_t>(this) + sizeof(Pose3D),
+				src = reinterpret_cast<uintptr_t>(&c) + sizeof(Pose3D);
+		std::memcpy(reinterpret_cast<void*>(ths), reinterpret_cast<const void*>(src), sizeof(CamData)-sizeof(Pose3D));
+	}
+	void CamData::moveFwd2D(float speed) {
+		const Quat& q = getRot();
+		Vec3 vZ = q.getZAxis();
+		vZ.y = 0;
+		vZ.normalize();
+		addOfsVec(vZ * speed);
+	}
+	void CamData::moveSide2D(float speed) {
+		const Quat& q = getRot();
+		Vec3 vX = q.getXAxis();
+		vX.y = 0;
+		vX.normalize();
+		addOfsVec(vX * speed);
+	}
+	void CamData::moveFwd3D(float speed) {
+		const Quat& q = getRot();
+		addOfsVec(q.getZAxis() * speed);
+	}
+	void CamData::moveSide3D(float speed) {
+		const Quat& q = getRot();
+		addOfsVec(q.getXAxis() * speed);
+	}
+	void CamData::turnAxis(const Vec3& axis, float rad) {
+		Quat q = getRot();
+		q.rotate(axis, rad);
+		setRot(q);
+	}
+	void CamData::turnYPR(float yaw, float pitch, float roll) {
+		Quat q = getRot();
+		q >>= Quat::RotationYPR(yaw, pitch, roll);
+		setRot(q);
+	}
+	void CamData::addRot(const Quat& q) {
+		Quat q0 = getRot();
+		q0 >>= q;
+		setRot(q0);
+	}
+	bool CamData::lerpTurn(const Quat& q_tgt, float t) {
+		Quat q = getRot();
+		q.lerp(q_tgt, t);
+		return q.distance(q_tgt) < TURN_THRESHOLD;
+	}
+	uint32_t CamData::getAccum() const {
+		return _accum + ((Pose3D*)this)->getAccum();
+	}
+
+	void CamData::setAspect(float ap) {
+		_aspect = ap;
+		++_accum;
+	}
+	void CamData::setFOV(float fv) {
+		_fov = fv;
+		++_accum;
+	}
+	void CamData::setZPlane(float n, float f) {
+		_nearZ = n;
+		_farZ = f;
+		++_accum;
+	}
+	void CamData::setNearDist(float n) {
+		_nearZ = n;
+		++_accum;
+	}
+	void CamData::setFarDist(float f) {
+		_farZ = f;
+		++_accum;
+	}
+
+	const AMat44& CamData::getViewProjMatrix() const {
+		_calcMatrices();
+		return _matVP;
+	}
+	const AMat43& CamData::getViewMatrix() const {
+		_calcMatrices();
+		return _matV;
+	}
+	const AMat44& CamData::getProjMatrix() const {
+		_calcMatrices();
+		return _matP;
+	}
+	const AMat44& CamData::getViewProjInv() const {
+		_calcVPInv();
+		return _matVPInv;
+	}
+
+	float CamData::getFOV() const {
+		return _fov;
+	}
+	float CamData::getAspect() const {
+		return _aspect;
+	}
+	float CamData::getFarDist() const {
+		return _farZ;
+	}
+	float CamData::getNearDist() const {
+		return _nearZ;
+	}
+	const Frustum& CamData::getFrustum() const {
+		_calcFrustum();
+		return _vfrus;
+	}
+	void CamData::unproject(Vec3& vdPos, const Vec3& vsPos) {
+		const AMat44& mI = getViewProjInv();
+		vdPos = AVec4(vsPos.asVec4(1) * mI).asVec3Coord();
+	}
+	void CamData::unprojectVec(Vec3& vdBegin, Vec3& vdEnd, const Vec2& vsPos) {
+		const AMat44& mI = getViewProjInv();
+		vdBegin = AVec4(AVec4(vsPos.x, vsPos.y, 0, 1) * mI).asVec3Coord();
+		vdEnd = AVec4(AVec4(vsPos.x, vsPos.y, 1, 1) * mI).asVec3Coord();
+	}
+	Vec3 CamData::vp2wp(float x, float y, float z) {
+		AVec4 v(x,y,z,1);
+		const AMat44& m = getViewProjMatrix();
+		AMat44 im;
+		m.inversion(im);
+		v *= im;
+
+		float inv = 1.0f/v.w;
+		return Vec3(v.x*inv, v.y*inv, v.z*inv);
+	}
+	Pose3D CamData::getPose() const {
+		return Pose3D(getOffset(), getRot(), AVec3(1,1,1));
+	}
+	void CamData::setPose(const Pose3D& ps) {
+		setOfsVec(ps.getOffset());
+		setRot(ps.getRot());
+		// (スケールは適用しない)
+	}
+	void CamData::adjustNoRoll() {
+		// X軸のY値が0になればいい
+
+		// 回転を一旦行列に直して軸を再計算
+		const auto& q = getRot();
+		auto rm = q.asMat33();
+		// Zはそのままに，X軸のY値を0にしてY軸を復元
+		Vec3 zA = rm.getRow(2),
+			xA = rm.getRow(0);
+		xA.y = 0;
+		if(xA.len_sq() < 1e-5f) {
+			// Xが真上か真下を向いている
+			float ang;
+			if(rm.ma[0][1] > 0) {
+				// 真上 = Z軸周りに右へ90度回転
+				ang = spn::DEGtoRAD(90);
+			} else {
+				// 真下 = 左へ90度
+				ang = spn::DEGtoRAD(-90);
+			}
+			setRot(AQuat::RotationZ(ang) * q);
+		} else {
+			xA.normalize();
+			Vec3 yA = zA % xA;
+			setRot(Quat::FromAxis(xA, yA, zA));
+		}
+	}
+	Plane CamData::getNearPlane() const {
+		Vec3 dir = getDir();
+		return Plane::FromPtDir(getOffset() + dir*getNearDist(), dir);
+	}
+	// カメラの変換手順としては offset -> rotation だがPose3Dの変換は rotation -> offsetなので注意！
+	Frustum CamData::getNearFrustum() const {
+		Frustum fr;
+		float t = std::tan(_fov/2);
+		fr.setScale(t*_aspect, t, getNearDist()*8);
+		fr.setRot(getRot());
+		fr.setOfsVec(getOffset());
+		return fr;
+	}
+}
--- camera.hpp
+++ camera.hpp
@@ -1,17 +1,96 @@
 #pragma once
 #include "spinner/pose.hpp"
+#include "spinner/plane.hpp"
+#include "spinner/quat.hpp"
+#include "spinner/resmgr.hpp"
+#include "boomstick/geom3D.hpp"
 
 namespace rs {
+	using spn::AMat43; using spn::AMat44;
+	using spn::AVec3; using spn::AVec4;
+	using spn::Vec2; using spn::Vec3;
+	using spn::Pose3D;
+	using spn::Plane;
+	using spn::Quat; using spn::AQuat;
+	using boom::geo3d::Frustum;
+
 	/*! 姿勢の保持はPose3Dクラスが行い，カメラ固有の変数だけを持つ */
-	class CamData : public spn::Pose3D, public spn::CheckAlign<16, CamData> {
-		GAP_MATRIX_DEF(multable, _matV, 3,4,
+	class CamData : public Pose3D, public spn::CheckAlign<16, CamData> {
+		GAP_MATRIX_DEF(mutable, _matV, 4,3,
 					   (((float, _fov)))
 					   (((float, _aspect)))
 					   (((float, _nearZ)))
 					   (((float, _farZ)))
 		)
-		mutable spn::AMat44	_matVP, _matP, _matVPInv;
-		// mutable VFrustum _vfrus;
+		// 変換キャッシュ
+		mutable AMat44	_matVP, _matP, _matVPInv;
+		mutable Frustum _vfrus;
+		// リフレッシュAccum値 (更新した時点の値)
+		mutable uint32_t 	_accum,
+							_acMat,
+							_acMatInv,
+							_acVFrus;
+		constexpr static float TURN_THRESHOLD = 1e-5f;
+		bool _checkAC(uint32_t& acDst) const;
+		void _calcMatrices() const;				// View,Proj,(Both)
+		void _calcVPInv() const;				// InverseMat(VP)
+		void _calcFrustum() const;
 
+		public:
+			CamData();
+			CamData(const CamData& c);
+
+			//! 前方への移動(XZ平面限定)
+			void moveFwd2D(float speed);
+			//! サイド移動(XZ平面限定)
+			void moveSide2D(float speed);
+			//! 前方への移動(軸フリー)
+			void moveFwd3D(float speed);
+			//! サイド移動(軸フリー)
+			void moveSide3D(float speed);
+			//! 方向転換(軸指定)
+			void turnAxis(const Vec3& axis, float rad);
+			//! Yaw Pitch Roll指定の回転
+			void turnYPR(float yaw, float pitch, float roll);
+			//! 差分入力
+			void addRot(const Quat& q);
+			//! 補間付き回転
+			/*! 3軸の目標距離を合計した物が閾値以下ならtrueを返す */
+			bool lerpTurn(const Quat& q_tgt, float t);
+			//! accumulation counter
+			uint32_t getAccum() const;
+
+			// アスペクト，FOV，Z平面
+			void setAspect(float ap);
+			void setFOV(float fv);
+			void setZPlane(float n, float f);
+			void setNearDist(float n);
+			void setFarDist(float f);
+			//! Upベクトルをrollが0になるよう補正
+			void adjustNoRoll();
+			// 各カメラ行列参照
+			const AMat44& getViewProjMatrix() const;
+			const AMat44& getProjMatrix() const;
+			const AMat43& getViewMatrix() const;
+			const AMat44& getViewProjInv() const;
+			// 各パラメータ取得
+			float getFOV() const;
+			float getAspect() const;
+			float getFarDist() const;
+			float getNearDist() const;
+			Plane getNearPlane() const;
+			const Frustum& getFrustum() const;
+			Frustum getNearFrustum() const;
+			// ビューポート座標をワールド座標系に変換
+			void unproject(Vec3& vdPos, const Vec3& vsPos);						// 単体
+			void unprojectVec(Vec3& vdBegin, Vec3& vdEnd, const Vec2& vsPos);	// (Near,Far)
+			// ビューポート座標からワールド座標(FarPlane位置)を取得
+			Vec3 vp2wp(float x, float y, float z);
+
+			Pose3D getPose() const;
+			void setPose(const Pose3D& ps);
 	};
+	#define mgr_cam (::rs::CameraMgr::_ref())
+	class CameraMgr : public spn::ResMgrA<CamData, CameraMgr> {};
+	DEF_HANDLE(CameraMgr, Cam, CamData)
 }
