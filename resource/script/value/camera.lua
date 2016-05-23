9a18da7479cf4a8bde9e6efbea977dca50ec5cab
degarashi <slice013@gmail.com>
2016-05-23 10:33:21 +0900

    Tweaker定義: Camera3D

---
+++ resource/script/value/camera.lua
@@ -0,0 +1,6 @@
+return {
+	fov = 60,
+	nearz = 1,
+	farz = 100,
+	offset = {1,0,1}
+}
--- resource/sys_script/Camera3D.lua
+++ resource/sys_script/Camera3D.lua
@@ -1,5 +1,47 @@
 require("sysfunc")
 return {
+	-- 実行時調整可能な値定義
+	_variable = {
+		-- 変数名
+		fov = {
+			-- 値をどのように適用するか
+			-- self: ターゲットオブジェクトハンドル
+			-- value: セットする値 (number or Vec[2-4])
+			apply = function(self, value)
+				self:setFov(Degree.New(value))
+			end,
+			-- 値操作の方式
+			manip = "linear",
+			-- 一回の操作で加える量
+			step = 1,
+			-- デフォルト値
+			default = 60
+		},
+		nearz = {
+			apply = function(self, value)
+				self:setNearZ(value)
+			end,
+			manip = "linear",
+			step = 1,
+			default = 1
+		},
+		farz = {
+			-- 文字列なら同名のcpp関数をそのまま呼ぶ仕様
+			apply = "setFarZ",
+			manip = "log",
+			step = 1,
+			default = 100
+		},
+		offset = {
+			apply = function(self, value)
+				local pose = self:refPose()
+				pose:setOffset(value)
+			end,
+			manip = "linear",
+			step = 1,
+			default = {1,0,1}
+		}
+	},
 	_renamefunc =
 		RS.RenameFunc(
 			{"setPose", "setPose<spn::Pose3D>"},
